4877cb32187859e325f752ce3df1280951c2fba0
priya-thv <piyaneha2002@gmail.com>
2022-11-20 13:35:57 +0530

    linkedlist

---
+++ lininkedlist1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+};
+
+int main(){
+    Node *head;
+    Node *temp;
+    Node *one=NULL;
+    Node *two=NULL;
+    Node *three=NULL;
+
+    //assigning location in memory
+    one=new Node();
+    two=new Node();
+    three=new Node();
+   //assigning 
+   one->data=3;
+   two->data=4;
+   three->data=6;
+
+   //connectiong
+   one->next=two;
+   two->next=three;
+   three->next=NULL;
+
+   head=one;
+    //traversing
+    temp=one;
+    cout<<"head->";
+
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }     cout<<"NULL";
+}
---
+++ queue.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+//complete the functions
+class Queue{
+	int A[100000];
+	int start;
+	int end;
+	
+	public:
+	//constructor is called whenever an object of the class is declared
+	Queue(){
+		cout<<"constructor is Called\n";
+		start=0;
+		end=0;
+	}
+	
+	//push the value at the back
+	void push(int val){
+		A[end]=val;
+		end++;
+		cout<<"pushing the val: "<<val<<" at the back of queue\n";
+	}
+	
+	int front(){
+		A[start];
+		cout<<"returning the value at front\n";
+		
+	}
+	
+	void pop(){
+		start++;
+		cout<<"removing the first element\n";
+	}
+	
+	int size(){
+
+		cout<<"return the size of the queue\n";
+	   
+	}
+	
+	bool isEmpty(){
+		if
+		cout<<"returning if the queue is empty\n";
+		 return true;
+	}
+};
+
+int main() {
+	Queue q; //constructor is called on this line
+	q.push(4);
+	cout<<"front: "<<q.front()<<"\n";
+	q.pop();
+	cout<<"size: "<<q.size()<<"\n";
+	cout<<"empty: "<<q.isEmpty()<<"\n";
+	
+	q.push(5);
+	q.pus
+	 h(3);
+	cout<<"front: "<<q.front()<<"\n";
+	q.pop();
+	cout<<"size: "<<q.size()<<"\n";
+	cout<<"empty: "<<q.isEmpty()<<"\n";
+	
+	cout<<"front: "<<q.front()<<"\n";
+	q.pop();
+	cout<<"size: "<<q.size()<<"\n";
+	cout<<"empty: "<<q.isEmpty()<<"\n";
+	return 0;
+}
