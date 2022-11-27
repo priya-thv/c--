88a4bc15c0f42fcb22e96a4cba0ade344c0be1fb
priya-thv <piyaneha2002@gmail.com>
2022-11-27 21:46:37 +0530

    insertathead

--- insertathead.cpp
+++ insertathead.cpp
@@ -1,58 +1,66 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct node{
+struct node
+{
     int data;
-    node*next;
-    node*prev;
+    node *next;
+    node *prev;
 };
 
-node *createnode(int data){
-    node *newnode=new node();
-    newnode->data=data;
+node *createnode(int data)
+{
+    node *newnode = new node();
+    newnode->data = data;
     return newnode;
 }
 
-void traverse(node *head){
-    cout<<"head->";
-    while(head!=NULL){
-        cout<<head->data<<"->";
-        head=head->next;
-    }cout<<"NULL";
-
+void traverse(node *head)
+{
+    cout << "null->";
+    while (head != NULL)
+    {
+        cout << head->data << "->";
+        head = head->next;
+    }
+    cout << "NULL";
 }
-void insertathead(node*prev,node*head,int data){
-    node *newnode=new node();
-    newnode->data=data;
+void insertathead(node *&head, int data)
+{
+    node *newnode = new node();
+    newnode->data = data;
     if(head==NULL){
         head=newnode;
     }else{
-        
-        
+        newnode->next=head;
+        head=newnode;
     }
 }
 
 
-
-int main(){
-    node *head=NULL;
-    node *temp=NULL;
+int main()
+{
+    node *head = NULL;
+    node *temp = NULL;
     int n;
-    cin>>n;
-    for(int i=0;i<n;i++)
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
         int t;
-        cin>>t;
-        if(head==NULL){
-            head=createnode(t);
-            temp=head;
-        }else{
-            temp->next=createnode(t);
-            temp=temp->next;
+        cin >> t;
+        if (head == NULL)
+        {
+            head = createnode(t);
+            temp = head;
         }
-    
+        else
+        {
+            temp->next = createnode(t);
+            temp = temp->next;
+        }
     }
     traverse(head);
-    cout<<endl;
-    insertathead(head,64);
+    cout << endl;
+    insertathead(head, 65);
+    traverse(head);
 }
